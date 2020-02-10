1c49c0af96d86efae16605585f0a9e80b2964167
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-02-11 00:10:39 +0100

    Pokegame Part3

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -19,14 +19,20 @@ class Pokegame extends Component{
         let hand2=[...this.props.pokemons];
         while(hand1.length < hand2.length){
             let randID = Math.floor(Math.random()*hand2.length);
-            let randPokemon = hand2.splice(randID,1);
+            let randPokemon = hand2.splice(randID,1)[0];
             hand1.push(randPokemon);
         }
-        console.log('hand1',hand1);
-        console.log('hand2',hand2);
+        //console.log('hand1',hand1);
+        //console.log('hand2',hand2);
+        let totalXp1 = hand1.reduce((xp,pokemon)=>xp+pokemon.base_experience,0);
+        let totalXp2 = hand2.reduce((xp,pokemon)=>xp+pokemon.base_experience,0);
+        console.log('exp1',totalXp1);
+        console.log('xp2',totalXp2);
         return(
             <div>
                 <h1>Pokegame</h1>
+                <Pokedex pokemons={hand1} xp={totalXp1} />
+                <Pokedex pokemons={hand2} xp={totalXp2} />
             </div>
         )
     }
