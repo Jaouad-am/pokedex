2bdc4d642c97ef758472a24bf50f6867cc697ffb
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-02-11 03:03:04 +0100

    Pokegame Component Part 1

---
+++ src/Pokegame.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+
+class Pokegame extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Pokegame</h1>
+            </div>
+        )
+    }
+}
+
+export default Pokegame;
