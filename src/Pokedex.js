2882f769399bc103d97cbd54f1d4f0e14bad8f32
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-02-13 06:06:47 +0100

    Styling Pokegame part1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,11 +4,18 @@ import './Pokedex.css';
 
 class Pokedex extends Component {
     render(){
+        let winner;
+        if(this.props.isWinner) {
+           winner = <h1 className='Pokedex-winner'>Winner !</h1>;
+        }
+        else {
+           winner = <h1 className='Pokedex-loser'>Loser !</h1>;
+        }
         return (
             <div className="Pokedex">
                 <h1>Pokedex</h1>
                 <p>Total Experience: {this.props.xp}</p>
-                <p>{this.props.isWinner ? "Winner" : "Loser"}</p> 
+                {winner}
                 <div className="Pokedex-cards">
                 {this.props.pokemons.map(p=>(
                     <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} xp={p.base_experience} />
